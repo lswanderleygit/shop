f2d805a4587214ff320b6b1d5654255aef8fd504
Lucas <lucaswanderley4@gmail.com>
2020-08-24 21:15:11 -0300

    create component product_item and replace on products_overview_screen

---
+++ lib/views/products_overview_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import '../data/dummy_data.dart';
+import '../models/product.dart';
+import '../widgets/product_item.dart';
+
+class ProductsOverviewScreen extends StatelessWidget {
+  final List<Product> loadedProducts = DUMMY_PRODUCTS;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Minha Loja'),
+      ),
+      body: GridView.builder(
+        itemCount: loadedProducts.length,
+        padding: const EdgeInsets.all(10),
+        itemBuilder: (ctx, index) => ProductItem(loadedProducts[index]),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+        ),
+      ),
+    );
+  }
+}
--- views/products_overview_screen.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class ProductsOverviewScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
