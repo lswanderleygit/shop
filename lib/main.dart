8ae49c10f5af6a71a4b623271d5c0f96d03144c6
Lucas <lucaswanderley4@gmail.com>
2020-08-31 20:35:32 -0300

    created screen to product detail, and navigation to product detail as well folder to routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:shop/views/product_detail_screen.dart';
+import 'utils/app_routes.dart';
 
 import './views/products_overview_screen.dart';
 
@@ -9,14 +11,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Minha Loja',
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
-        accentColor: Colors.orange,
-        fontFamily: 'Lato',
-      ),
-      home: ProductsOverviewScreen(),
-    );
+        title: 'Minha Loja',
+        theme: ThemeData(
+          primarySwatch: Colors.purple,
+          accentColor: Colors.orange,
+          fontFamily: 'Lato',
+        ),
+        home: ProductsOverviewScreen(),
+        routes: {
+          AppRoutes.PRODUCT_DETAIL: (ctx) => ProductDetailScreen(),
+        });
   }
 }
 
