34d015498f7102c60770d5e8dcb5f5905d2b54b8
Lucas <lucaswanderley4@gmail.com>
2020-08-31 21:20:07 -0300

    create provider and how to use inheritedWidget

--- lib/main.dart
+++ lib/main.dart
@@ -4,23 +4,26 @@ import 'package:shop/views/product_detail_screen.dart';
 import 'utils/app_routes.dart';
 
 import './views/products_overview_screen.dart';
+import './providers/counter_providers.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Minha Loja',
-        theme: ThemeData(
-          primarySwatch: Colors.purple,
-          accentColor: Colors.orange,
-          fontFamily: 'Lato',
-        ),
-        home: ProductsOverviewScreen(),
-        routes: {
-          AppRoutes.PRODUCT_DETAIL: (ctx) => ProductDetailScreen(),
-        });
+    return CounterProvider(
+      child: MaterialApp(
+          title: 'Minha Loja',
+          theme: ThemeData(
+            primarySwatch: Colors.purple,
+            accentColor: Colors.orange,
+            fontFamily: 'Lato',
+          ),
+          home: ProductsOverviewScreen(),
+          routes: {
+            AppRoutes.PRODUCT_DETAIL: (ctx) => ProductDetailScreen(),
+          }),
+    );
   }
 }
 
