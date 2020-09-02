35cf12821053ff6267945f37aec3aadb2bbb9d4d
Lucas <lucaswanderley4@gmail.com>
2020-09-01 21:23:30 -0300

    model of provider

--- lib/views/product_detail_screen.dart
+++ lib/views/product_detail_screen.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 import '../models/product.dart';
+import '../providers/counter_providers.dart';
 
-class ProductDetailScreen extends StatelessWidget {
+class ProductDetailScreen extends StatefulWidget {
+  @override
+  _ProductDetailScreenState createState() => _ProductDetailScreenState();
+}
+
+class _ProductDetailScreenState extends State<ProductDetailScreen> {
   @override
   Widget build(BuildContext context) {
     final Product product =
@@ -10,6 +16,20 @@ class ProductDetailScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text(product.title),
+      ),
+      body: Column(
+        children: <Widget>[
+          Text(CounterProvider.of(context).state.value.toString()),
+          RaisedButton(
+            child: Text("+"),
+            onPressed: () {
+              setState(() {
+                CounterProvider.of(context).state.inc();
+              });
+              print(CounterProvider.of(context).state.value);
+            },
+          )
+        ],
       ),
     );
   }
