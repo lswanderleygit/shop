4dc0b392a4b0f1b640652a4bee1fb2758a14bff2
Lucas <lucaswanderley4@gmail.com>
2020-09-02 20:24:49 -0300

    removed the code about providers

--- lib/views/product_detail_screen.dart
+++ lib/views/product_detail_screen.dart
@@ -1,13 +1,7 @@
 import 'package:flutter/material.dart';
 import '../models/product.dart';
-import '../providers/counter_providers.dart';
 
-class ProductDetailScreen extends StatefulWidget {
-  @override
-  _ProductDetailScreenState createState() => _ProductDetailScreenState();
-}
-
-class _ProductDetailScreenState extends State<ProductDetailScreen> {
+class ProductDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final Product product =
@@ -16,20 +10,6 @@ class _ProductDetailScreenState extends State<ProductDetailScreen> {
     return Scaffold(
       appBar: AppBar(
         title: Text(product.title),
-      ),
-      body: Column(
-        children: <Widget>[
-          Text(CounterProvider.of(context).state.value.toString()),
-          RaisedButton(
-            child: Text("+"),
-            onPressed: () {
-              setState(() {
-                CounterProvider.of(context).state.inc();
-              });
-              print(CounterProvider.of(context).state.value);
-            },
-          )
-        ],
       ),
     );
   }
