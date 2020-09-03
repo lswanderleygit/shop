8d4702d21cbf41f93386c819fc8c3ceb0b6bc9da
Lucas <lucaswanderley4@gmail.com>
2020-09-03 20:56:33 -0300

    create product grid widget

--- lib/views/products_overview_screen.dart
+++ lib/views/products_overview_screen.dart
@@ -1,11 +1,8 @@
 import 'package:flutter/material.dart';
-import '../data/dummy_data.dart';
-import '../models/product.dart';
-import '../widgets/product_item.dart';
+
+import '../widgets/product_grid.dart';
 
 class ProductsOverviewScreen extends StatelessWidget {
-  final List<Product> loadedProducts = DUMMY_PRODUCTS;
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,17 +11,7 @@ class ProductsOverviewScreen extends StatelessWidget {
           child: Text('Minha Loja'),
         ),
       ),
-      body: GridView.builder(
-        itemCount: loadedProducts.length,
-        padding: const EdgeInsets.all(10),
-        itemBuilder: (ctx, index) => ProductItem(loadedProducts[index]),
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 10,
-          mainAxisSpacing: 10,
-        ),
-      ),
+      body: ProductGrid(),
     );
   }
 }
---
+++ lib/widgets/product_grid.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../models/product.dart';
+import '../widgets/product_item.dart';
+import '../providers/products.dart';
+
+class ProductGrid extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final List<Product> loadedProducts = Provider.of<Products>(context).items;
+    return GridView.builder(
+      itemCount: loadedProducts.length,
+      padding: const EdgeInsets.all(10),
+      itemBuilder: (ctx, index) => ProductItem(loadedProducts[index]),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+      ),
+    );
+  }
+}
