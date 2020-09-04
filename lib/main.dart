de073acdad6ef4691787f142e4ab554e6b607837
Lucas <lucaswanderley4@gmail.com>
2020-09-03 21:00:07 -0300

    add provide dependence and add wrapper material app to notify provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,30 @@
-import 'package:flutter/cupertino.dart';
+import 'package:provider/provider.dart';
 import 'package:flutter/material.dart';
-import 'package:shop/views/product_detail_screen.dart';
-import 'utils/app_routes.dart';
 
+import './views/product_detail_screen.dart';
+import './utils/app_routes.dart';
 import './views/products_overview_screen.dart';
+import './providers/products.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Minha Loja',
-        theme: ThemeData(
-          primarySwatch: Colors.purple,
-          accentColor: Colors.orange,
-          fontFamily: 'Lato',
-        ),
-        home: ProductsOverviewScreen(),
-        routes: {
-          AppRoutes.PRODUCT_DETAIL: (ctx) => ProductDetailScreen(),
-        });
+    return ChangeNotifierProvider(
+      create: (_) => Products(),
+      child: MaterialApp(
+          title: 'Minha Loja',
+          theme: ThemeData(
+            primarySwatch: Colors.purple,
+            accentColor: Colors.orange,
+            fontFamily: 'Lato',
+          ),
+          home: ProductsOverviewScreen(),
+          routes: {
+            AppRoutes.PRODUCT_DETAIL: (ctx) => ProductDetailScreen(),
+          }),
+    );
   }
 }
 
