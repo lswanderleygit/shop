0417c74fe3c9bdf3dfa8ae6c5d6b5cf25ec15133
Lucas <lucaswanderley4@gmail.com>
2020-09-06 21:54:47 -0300

    use consumer to get product list of provider and create favorite filter to controlled products on all application

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -4,7 +4,25 @@ import '../data/dummy_data.dart';
 
 class Products with ChangeNotifier {
   List<Product> _items = DUMMY_PRODUCTS;
-  List<Product> get items => [..._items];
+
+  bool _showFavoriteOnly = false;
+
+  List<Product> get items {
+    if (_showFavoriteOnly) {
+      return _items.where((product) => product.isFavorite).toList();
+    }
+    return [..._items];
+  }
+
+  void showFavoriteOnly() {
+    _showFavoriteOnly = true;
+    notifyListeners();
+  }
+
+  void showAll() {
+    _showFavoriteOnly = false;
+    notifyListeners();
+  }
 
   void addProduct(Product product) {
     _items.add(product);
--- lib/views/products_overview_screen.dart
+++ lib/views/products_overview_screen.dart
@@ -1,15 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../widgets/product_grid.dart';
+import '../providers/products.dart';
+
+enum FilterOptions { Favorite, All }
 
 class ProductsOverviewScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final Products products = Provider.of(context);
+
     return Scaffold(
       appBar: AppBar(
         title: Center(
           child: Text('Minha Loja'),
         ),
+        actions: <Widget>[
+          PopupMenuButton(
+            onSelected: (FilterOptions selectedValue) {
+              if (selectedValue == FilterOptions.Favorite) {
+                products.showFavoriteOnly();
+              } else {
+                products.showAll();
+              }
+            },
+            icon: Icon(Icons.more_vert),
+            itemBuilder: (_) => [
+              PopupMenuItem(
+                child: Text('Somente Favoritos'),
+                value: FilterOptions.Favorite,
+              ),
+              PopupMenuItem(
+                child: Text('Todos'),
+                value: FilterOptions.All,
+              ),
+            ],
+          )
+        ],
       ),
       body: ProductGrid(),
     );
