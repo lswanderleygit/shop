060d93967958b9141e060c4a6c245e38770127e6
Lucas <lucaswanderley4@gmail.com>
2020-09-07 01:18:01 -0300

    added multiple provider to add both provider to cart and products

--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,24 @@ import 'package:flutter/material.dart';
 import './views/product_detail_screen.dart';
 import './utils/app_routes.dart';
 import './views/products_overview_screen.dart';
+
 import './providers/products.dart';
+import './providers/cart.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (_) => Products(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => new Products(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => new Cart(),
+        ),
+      ],
       child: MaterialApp(
           title: 'Minha Loja',
           theme: ThemeData(
