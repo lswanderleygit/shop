d441bbc01a7c8cba85a8eb667bab406ab7ebb6aa
Lucas <lucaswanderley4@gmail.com>
2020-09-12 01:10:27 -0300

    create drawer

---
+++ lib/widgets/app_drawer.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+import '../utils/app_routes.dart';
+
+class AppDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: <Widget>[
+          AppBar(
+            centerTitle: true,
+            automaticallyImplyLeading: false,
+            title: Text('Bem vindo Usuário'),
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.shop),
+            title: Text('Loja'),
+            onTap: () {
+              Navigator.of(context).pushReplacementNamed(AppRoutes.HOME);
+            },
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.payment),
+            title: Text('Pedidos'),
+            onTap: () {
+              Navigator.of(context).pushReplacementNamed(AppRoutes.ORDERS);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
