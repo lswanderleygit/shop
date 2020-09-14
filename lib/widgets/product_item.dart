0948ed412b3006b20f20f5a58e54b2849cdd79ed
Lucas <lucaswanderley4@gmail.com>
2020-09-13 22:29:46 -0300

    create widget to product item

---
+++ lib/widgets/product_item.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+import '../providers/product.dart';
+
+class ProductItem extends StatelessWidget {
+  final Product product;
+
+  ProductItem(this.product);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+        leading: CircleAvatar(
+          backgroundImage: NetworkImage(product.imageUrl),
+        ),
+        title: Text(product.title),
+        trailing: Container(
+          width: 100,
+          child: Row(
+            children: <Widget>[
+              IconButton(
+                icon: Icon(Icons.edit),
+                onPressed: () {},
+              ),
+              IconButton(
+                icon: Icon(
+                  Icons.delete,
+                  color: Colors.redAccent,
+                ),
+                onPressed: () {},
+              )
+            ],
+          ),
+        ));
+  }
+}
