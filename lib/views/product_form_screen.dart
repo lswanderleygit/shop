3a5bd1d354f405cbf0e8717356eb7c3d000490f9
Lucas <lucaswanderley4@gmail.com>
2020-09-15 20:08:23 -0300

    add life cicle to update setState according with controller

--- lib/views/product_form_screen.dart
+++ lib/views/product_form_screen.dart
@@ -8,6 +8,29 @@ class ProductFormScreen extends StatefulWidget {
 
 class _ProductFormScreenState extends State<ProductFormScreen> {
   final _priceFocosNode = FocusNode();
+  final _descriptionFocosNode = FocusNode();
+  final _imageUrlFocosNode = FocusNode();
+  final _imageUrlCoontroller = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _imageUrlFocosNode.addListener(_updateImage);
+  }
+
+  void _updateImage() {
+    // update component when exit end get most recent updates
+    setState(() {});
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _priceFocosNode.dispose();
+    _descriptionFocosNode.dispose();
+    _descriptionFocosNode.removeListener(_updateImage);
+    _imageUrlFocosNode.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -33,6 +56,48 @@ class _ProductFormScreenState extends State<ProductFormScreen> {
                 textInputAction: TextInputAction.next,
                 focusNode: _priceFocosNode,
                 keyboardType: TextInputType.numberWithOptions(decimal: true),
+                onFieldSubmitted: (_) {
+                  FocusScope.of(context).requestFocus(_descriptionFocosNode);
+                },
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Descrição'),
+                keyboardType: TextInputType.multiline,
+                maxLines: 3,
+                // textInputAction: TextInputAction.next,
+                focusNode: _descriptionFocosNode,
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: <Widget>[
+                  Expanded(
+                    child: TextFormField(
+                      decoration: InputDecoration(labelText: 'URL da Imagem'),
+                      keyboardType: TextInputType.url,
+                      textInputAction: TextInputAction.done,
+                      focusNode: _imageUrlFocosNode,
+                      controller: _imageUrlCoontroller,
+                    ),
+                  ),
+                  Container(
+                    height: 100,
+                    width: 100,
+                    margin: EdgeInsets.only(top: 8, left: 10),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.grey,
+                        width: 1,
+                      ),
+                    ),
+                    alignment: Alignment.center,
+                    child: _imageUrlCoontroller.text.isEmpty
+                        ? Text("Informe a URL")
+                        : FittedBox(
+                            child: Image.network(_imageUrlCoontroller.text),
+                            fit: BoxFit.cover,
+                          ),
+                  ),
+                ],
               ),
             ],
           ),
