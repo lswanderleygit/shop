f818956f4797f831bbaa572f18b9935b9e48ea37
Lucas <lucaswanderley4@gmail.com>
2020-09-19 15:39:48 -0300

    confirm delete product

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/providers/products.dart';
 import 'package:shop/utils/app_routes.dart';
 
 import '../providers/product.dart';
@@ -33,7 +35,36 @@ class ProductItem extends StatelessWidget {
                   Icons.delete,
                   color: Colors.redAccent,
                 ),
-                onPressed: () {},
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                    builder: (ctx) => AlertDialog(
+                      title: Text('Tem certeza?'),
+                      content: Text('Você quer deletar esse produto?'),
+                      actions: <Widget>[
+                        FlatButton(
+                          onPressed: () {
+                            Navigator.of(ctx).pop(true);
+                            // Provider.of<Products>(context, listen: false)
+                            //     .deleteProduct(product.id);
+                          },
+                          child: Text('Sim'),
+                        ),
+                        FlatButton(
+                          onPressed: () {
+                            Navigator.of(ctx).pop(false);
+                          },
+                          child: Text('Não'),
+                        )
+                      ],
+                    ),
+                  ).then((value) {
+                    if (value) {
+                      Provider.of<Products>(context, listen: false)
+                          .deleteProduct(product.id);
+                    }
+                  });
+                },
               )
             ],
           ),
